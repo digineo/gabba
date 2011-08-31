83090ef247dab395c6c70a0df9f6682ce5ea4c5c
Julian Kornberger <jk+github@digineo.de>
2011-08-31 19:21:20 +0200

    tracking might work now

--- lib/gabba/gabba.rb
+++ lib/gabba/gabba.rb
@@ -4,6 +4,13 @@ require "net/http"
 require 'cgi'
 require File.dirname(__FILE__) + '/version'
 
+# 
+# Inspired by
+#  https://gist.github.com/533053
+# 
+# The GIF Request Parameters:
+#  https://code.google.com/intl/de-DE/apis/analytics/docs/tracking/gaTrackingTroubleshooting.html#gifParameters
+# 
 module Gabba
   
   class NoGoogleAnalyticsAccountError < RuntimeError; end
@@ -19,17 +26,16 @@ module Gabba
     cattr_accessor :async
     self.async = true
     
-    attr_accessor :utmwv, :utmn, :utmhn, :utmcs, :utmul, :utmdt, :utmp, :utmac, :utmt, :utmcc, :user_agent
+    attr_accessor :utmwv, :utmn, :utmhn, :utmcs, :utmdt, :utmp, :utmac, :user_agent
     
-    def initialize(ga_acct, domain, agent = Gabba::USER_AGENT)
+    def initialize(account, domain, agent = Gabba::USER_AGENT)
+      raise NoGoogleAnalyticsAccountError if account !~ /^MO-\d+-\d+$/
+      raise NoGoogleAnalyticsDomainError if domain.blank?
+      
       @utmwv = "4.4sh" # GA version
       @utmcs = "UTF-8" # charset
-      @utmul = "en-us" # language
-       
-      @utmn = random_id
-      @utmhid = random_id
-      
-      @utmac = ga_acct
+      @utmn  = random_id # Unique ID (random number) generated for each request
+      @utmac = account # account string (MO-xxxxxxx-x)
       @utmhn = domain
       @user_agent = agent
     end
@@ -39,27 +45,26 @@ module Gabba
     # page - Path (URI) of the current page
     # params - Additional params for tracking, i.e.:
     #   :utmip  - the client's IP address
-    #   :utmvid - the unique visitor's ID
     def page_view(title, page, params={})
-      check_account_params
-      hey(page_view_params(title, page).merge(params))
+      hey page_view_params(title, page, params)
     end
 
-    def page_view_params(title, page)
-      default_params.merge \
+    def page_view_params(title, page, params)
+      build_params params.merge(
         :utmdt => title,
-        :utmp => page
+        :utmp  => page
+      )
     end
   
     def event(category, action, label = nil, value = nil, params = {})
-      check_account_params
-      hey(event_params(category, action, label, value).merge(params))
+      hey event_params(category, action, label, value, params)
     end
 
-    def event_params(category, action, label = nil, value = nil)
-      default_params.merge \
+    def event_params(category, action, label = nil, value = nil, params)
+      build_params params.merge(
         :utmt => 'event',
         :utme => event_data(category, action, label, value)
+      )
     end
 
     def event_data(category, action, label = nil, value = nil)
@@ -69,11 +74,10 @@ module Gabba
     end
     
     def transaction(order_id, total, store_name = nil, tax = nil, shipping = nil, city = nil, region = nil, country = nil, params = {})
-      check_account_params
-      hey(transaction_params(order_id, total, store_name, tax, shipping, city, region, country).merge(params))
+      hey transaction_params(order_id, total, store_name, tax, shipping, city, region, country, params)
     end
 
-    def transaction_params(order_id, total, store_name, tax, shipping, city, region, country)
+    def transaction_params(order_id, total, store_name, tax, shipping, city, region, country, params)
       # '1234',           // utmtid URL-encoded order ID - required
       # 'Acme Clothing',  // utmtst affiliation or store name
       # '11.99',          // utmtto total - required
@@ -82,9 +86,8 @@ module Gabba
       # 'San Jose',       // utmtci city
       # 'California',     // utmtrg state or province
       # 'USA'             // utmtco country
-      default_params.merge \
+      build_params params.merge(
         :utmt => 'tran',
-        :utmhid => utmhid,
         :utmtid => order_id,
         :utmtst => store_name,
         :utmtto => total,
@@ -93,55 +96,44 @@ module Gabba
         :utmtci => city,
         :utmtrg => region,
         :utmtco => country
+      )
     end
     
     def add_item(order_id, item_sku, price, quantity, name = nil, category = nil, params = {})
-      check_account_params
-      hey(item_params(order_id, item_sku, name, category, price, quantity).merge(params))
+      hey item_params(order_id, item_sku, name, category, price, quantity, params)
     end
     
-    def item_params(order_id, item_sku, name, category, price, quantity)
+    def item_params(order_id, item_sku, name, category, price, quantity, params)
       # '1234',           // utmtid URL-encoded order ID - required
       # 'DD44',           // utmipc SKU/code - required
       # 'T-Shirt',        // utmipn product name
       # 'Green Medium',   // utmiva category or variation
       # '11.99',          // utmipr unit price - required
       # '1'               // utmiqt quantity - required
-      default_params.merge \
-        :utmt => 'item',
+      build_params params.merge(
+        :utmt   => 'item',
         :utmtid => order_id,
         :utmipc => item_sku,
         :utmipn => name,
         :utmiva => category,
         :utmipr => price,
         :utmiqt => quantity
-    end
-  
-    # create magical cookie params used by GA for its own nefarious purposes
-    def cookie_params(utma1 = random_id, utma2 = rand(1147483647) + 1000000000, today = Time.now)
-      "__utma=1.#{utma1}00145214523.#{utma2}.#{today.to_i}.#{today.to_i}.15;+__utmz=1.#{today.to_i}.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none);"
+      )
     end
     
     protected
     
-    def default_params
+    def build_params(h)
       {
+        :utmac => @utmac,
         :utmwv => @utmwv,
-        :utmn => @utmn,
+        :utmn  => @utmn,
         :utmhn => @utmhn,
         :utmcs => @utmcs,
-        :utmul => @utmul,
-        :utmac => @utmac,
-        :utmcc => @utmcc || cookie_params
-      }
+        :utmcc => '__utma=999.999.999.999.999.1;'
+      }.merge(h)
     end
 
-    # sanity check that we have needed params to even call GA
-    def check_account_params
-      raise NoGoogleAnalyticsAccountError unless @utmac
-      raise NoGoogleAnalyticsDomainError unless @utmhn
-    end
-    
     def hey(params)
       if async
         Thread.new { hey_sync(params) }
@@ -152,6 +144,8 @@ module Gabba
     
     # makes the tracking call to Google Analytics
     def hey_sync(params)
+      user_agent = params.delete(:user_agent) || user_agent
+      
       query = params.map {|k,v| "#{k}=#{URI.escape(v.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}" }.join('&')
       response = Net::HTTP.start(GOOGLE_HOST) do |http|
         request = Net::HTTP::Get.new("#{BEACON_PATH}?#{query}")
